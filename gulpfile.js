376ff19e7ec7844fc78de66565caf910da97625f
Alabê Duarte <aduarte@thoughtworks.com>
2015-04-06 18:22:18 -0300

    Move bower dependencies installation to be managed by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
+var bower = require('gulp-bower');
 
 gulp.task('default', function(){
   return gulp.src('jsx/*.jsx')
     .pipe(react())
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(bower());
 });
