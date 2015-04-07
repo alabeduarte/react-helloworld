b557c116ff5cf85cbd8302f1609395e50eb9d6c4
Alabê Duarte <aduarte@thoughtworks.com>
2015-04-07 11:41:41 -0300

    Handling post request for new comments

--- public/jsx/commentBox.jsx
+++ public/jsx/commentBox.jsx
@@ -1,9 +1,23 @@
 var CommentBox = React.createClass({
   loadComments: function() {
     $.get(this.props.url, function(data) {
-      console.log('√', data);
       this.setState({ data: data });
     }.bind(this));
+  },
+  handleCommentSubmit: function(comment) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        console.log('√', data);
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error("<!>", this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
   getInitialState: function() {
     return { data: [] };
@@ -16,7 +30,7 @@ var CommentBox = React.createClass({
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('port', process.env.PORT || 9000);
 app.use(express.static('public'));
 
+var data = [
+  { "id": "1", "author": "Pete Hunt", "text": "This is one comment" },
+  { "id": "2", "author": "Jordan Walke", "text": "This is *another* comment" }
+];
+
 app.get('/comments.json', function(req, res) {
-  res.send(
-    [
-      { "id": "1", "author": "Pete Hunt", "text": "This is one comment" },
-      { "id": "2", "author": "Jordan Walke", "text": "This is *another* comment" }
-    ]
-  );
+  res.send(data);
+});
+
+app.post('/comments.json', function(req, res) {
+  newComment = req.body;
+  console.log('√', newComment);
+  data.push(newComment);
+
+  res.send(data);
 });
 
 var server = app.listen(app.get('port'), function() {
